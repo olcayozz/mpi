cf4e7566b4aca36dec80a05ca9d8ac7844e15179
Olcay Ozyilmaz <olcayozz@hotmail.com>
2016-05-30 06:05:53 -0700

    mpi matris multiplation

---
+++ src/MPI.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : MPI.c
+ Author      : Olcay Ozyilmaz
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hi"); /* prints Hi */
+	return EXIT_SUCCESS;
+}
